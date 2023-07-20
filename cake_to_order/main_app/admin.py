3ef541ce60849200e530715973c352984e4f2f5a
Vladimir <vladimir.papin@gmail.com>
2023-07-20 17:41:54 +0300

    added base

--- cake_to_order/main_app/admin.py
+++ cake_to_order/main_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Client, Cake, Topping, Berry, Decor, CakeLevel, CakeForm
+
+
+admin.site.register(Client)
+admin.site.register(Cake)
+admin.site.register(Topping)
+admin.site.register(Berry)
+admin.site.register(Decor)
+admin.site.register(CakeLevel)
+admin.site.register(CakeForm)
 
 # Register your models here.
--- cake_to_order/main_app/models.py
+++ cake_to_order/main_app/models.py
@@ -4,7 +4,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
-class ClientUser(models.Model):
+class Client(models.Model):
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
@@ -18,8 +18,12 @@ class ClientUser(models.Model):
     phone = PhoneNumberField(
         'Номер телефона',
         null=True,
-        blank=True,
-        db_index=True)
+        blank=True)
+    adress = models.CharField(
+        'Адрес',
+        max_length=50,
+        null=True,
+        blank=True)
 
 
     class Meta:
@@ -29,3 +33,125 @@ class ClientUser(models.Model):
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
+
+
+class Cake(models.Model):
+    title = models.CharField(
+        'Название',
+        max_length=50)
+    description = models.TextField(
+        'Описание',)
+    price = models.DecimalField(
+        'Цена',
+        max_digits=7,
+        decimal_places=2)
+    weight = models.DecimalField(
+        'Вес',
+        max_digits=4,
+        decimal_places=2)
+    image = models.ImageField(
+        'Картинка',
+        upload_to='cakes_img')
+
+
+    class Meta:
+        verbose_name='Торт'
+        verbose_name_plural='Торты'
+
+
+    def __str__(self):
+        return f'{self.title}, {self.weight} кг., {self.price} р.'
+
+
+class Topping(models.Model):
+    title = models.CharField(
+        'Название',
+        max_length=50)
+    price = models.DecimalField(
+        'Цена',
+        max_digits=7,
+        decimal_places=2)
+
+
+    class Meta:
+        verbose_name='Топпинг'
+        verbose_name_plural='Топпинги'
+
+
+    def __str__(self):
+        return f'{self.title}, {self.price} р.'
+
+
+class Berry(models.Model):
+    title = models.CharField(
+        'Название',
+        max_length=50)
+    price = models.DecimalField(
+        'Цена',
+        max_digits=7,
+        decimal_places=2)
+
+
+    class Meta:
+        verbose_name='Ягода'
+        verbose_name_plural='Ягоды'
+
+
+    def __str__(self):
+        return f'{self.title}, {self.price} р.'
+
+
+class Decor(models.Model):
+    title = models.CharField(
+        'Название',
+        max_length=50)
+    price = models.DecimalField(
+        'Цена',
+        max_digits=7,
+        decimal_places=2)
+
+
+    class Meta:
+        verbose_name='Декор'
+        verbose_name_plural='Декор'
+
+
+    def __str__(self):
+        return f'{self.title}, {self.price} р.'
+
+class CakeLevel(models.Model):
+    title = models.CharField(
+        'Название',
+        max_length=50)
+    price = models.DecimalField(
+        'Цена',
+        max_digits=7,
+        decimal_places=2)
+
+
+    class Meta:
+        verbose_name='Уровень'
+        verbose_name_plural='Уровни'
+
+
+    def __str__(self):
+        return f'{self.title}, {self.price} р.'
+
+
+class CakeForm(models.Model):
+    title = models.CharField(
+        'Название',
+        max_length=50)
+    price = models.DecimalField(
+        'Цена',
+        max_digits=7,
+        decimal_places=2)
+
+
+    class Meta:
+        verbose_name='Форма'
+        verbose_name_plural='Формы'
+
+
+    def __str__(self):
+        return f'{self.title}, {self.price} р.'
