0fdfeba14a77ab83c2c66afbf9d3cc064c06f869
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-07-23 10:10:39 +0300

    Update settings.py

--- cake_to_order/cake_to_order/settings.py
+++ cake_to_order/cake_to_order/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS", default='localhost')
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default='localhost')
 
 TG_BOT_TOKEN = env.str("TG_TOKEN")
 
