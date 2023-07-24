c6f3a086ea3195cda275456b2c2226d7410c3971
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-07-24 11:30:07 +0300

    fix settings

--- cake_to_order/cake_to_order/settings.py
+++ cake_to_order/cake_to_order/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -71,7 +72,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "whitenoise.middleware.WhiteNoiseMiddleware",
             ],
         },
     },
