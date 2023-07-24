44ab45a9ce3f9aeef5ccd1929b7ea5db0cdd995a
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-07-24 10:33:24 +0300

    add server.py for waitress server

--- cake_to_order/cake_to_order/wsgi.py
+++ cake_to_order/cake_to_order/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cake_to_order.settings")
-
-application = get_wsgi_application()
+application = WhiteNoise(get_wsgi_application())
