1356959b82bb5774023e5270bcd478ff31dfe435
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-07-24 10:57:03 +0300

    update settings.py

--- cake_to_order/cake_to_order/wsgi.py
+++ cake_to_order/cake_to_order/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from settings import BASE_DIR
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cake_to_order.settings")
-application = WhiteNoise(get_wsgi_application())
+application = WhiteNoise(get_wsgi_application(), root=(os.path.join(BASE_DIR, 'static')))
